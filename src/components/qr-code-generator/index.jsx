c096dde0d1fd1a1f6f5349f0a1acc9bd610c8de6
Facundo Herrera <facundhfed@gmail.com>
2024-02-19 06:51:46 +0100

    new hooks in the component

--- src/components/qr-code-generator/index.jsx
+++ src/components/qr-code-generator/index.jsx
@@ -1,19 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import QRCode from 'react-qr-code'
 
 const QrCodeGenerator = () => {
     const [qrCode, setQrCode] = useState('');
     const [input, setInput] = useState('');
+    const inputref = useRef(null)
 
     const handleGenerateQrCode = () => {
         setQrCode(input)
+        setInput('');
     }
    
+    useEffect(() => {
+        inputref.current.focus()
+    }, [handleGenerateQrCode])
   return (
     <div>
       <h1>Qr Code Generator</h1>
       <div>
-        <input onChange={(e) => setInput(e.target.value)} type="text" name='qr-code' placeholder='Enter your value' />
+        <input onChange={(e) => setInput(e.target.value)} type="text" name='qr-code' placeholder='Enter your value' value={input} ref={inputref}/>
         <button disabled={input && input.trim() !== '' ? false : true } onClick={handleGenerateQrCode}>Generate</button>
         <div>
             <QRCode id='qr-code-value' value={qrCode}  size={400}/>
